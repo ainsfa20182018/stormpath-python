e515d295b6bc0dbb03fdafdedb3a9336b605592f
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-05-04 16:09:42 -0500

    add

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,12 +2,26 @@ node{
     properties([parameters([string(defaultValue: 'IP', description: 'build IP', name: 'ENV', trim: true)])])
     stage("Clone repo"){
         git 'https://github.com/ainsfa20182018/stormpath-python.git'
-     }
-    stage("Build application"){
+    }
+    stage("Remove /tmp content"){
+        sh "ssh ec2-user@${ENV} sudo rm -rf /tmp/*"
+    }
+    stage("Copy filese over"){
         sh "scp -r * ec2-user@${ENV}:/tmp"
-        sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("Create Folder"){        
+        sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
+    }
+    stage("Copy to System"){
+        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/flaskex.service /etc/systemd/system"
+    }    
+    stage("move files to /flaskex"){
+        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
+    }
+    stage("Install requiremennts"){
+        sh "ssh ec2-user@${ENV} sudo pip install -r /flaskex/requirements.txt"
     }
     stage("App Run"){
-        sh "ssh ec2-user@${ENV}  python  /tmp/app.py"
+        sh "ssh ec2-user@${ENV}  systemctl start flaskex"
     }
 }
