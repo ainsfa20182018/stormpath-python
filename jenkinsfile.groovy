56b8549201e515708739065d5f80b0cf4b66d2d9
ainsfa20182018 <ainsfa20182018@gmail.com>
2019-05-04 16:33:13 -0500

    add

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,11 +12,28 @@ node{
     stage("Create Folder"){        
         sh "ssh ec2-user@${ENV} sudo mkdir -p /flaskex"
     }
+    //Block to work on next    
+    stage("Write to a file"){
+        sh "ssh ec2-user@${ENV} echo [Unit] > /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo After=network.target >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo [Service] >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo Type=simple >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo ExecStart=/bin/python /flaskex/app.py  >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo Restart=on-abort >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo [Install] >> /tmp/flaskex.service"
+        sh "ssh ec2-user@${ENV} echo WantedBy=multi-user.target >> /tmp/flaskex.service"
+    }
+    //Block to work on next
     stage("Copy to System"){
         sh "ssh ec2-user@${ENV} sudo cp -r /tmp/flaskex.service /etc/systemd/system"
     }    
     stage("move files to /flaskex"){
-        sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
+        try {
+            sh "ssh ec2-user@${ENV} sudo cp -r /tmp/* /flaskex"
+        }
+        catch(err){
+            sh "echo did not copy"
+        }
     }
     stage("Install requiremennts"){
         sh "ssh ec2-user@${ENV} sudo pip install -r /flaskex/requirements.txt"
@@ -25,3 +42,4 @@ node{
         sh "ssh ec2-user@${ENV}  systemctl start flaskex"
     }
 }
+© 2019 GitHub, Inc.
